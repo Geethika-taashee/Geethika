556a4e9107b33149930abd11e3a3c75ebde3fa35
Geethika-taashee <geetika.a@taashee.com>
2025-02-20 17:08:30 +0530

    Delete old file

--- random_password.py
+++
@@ -1,5 +0,0 @@
-import random
-import string
-length=int(input("Enter password length:"))
-password=''.join(random.choices(string.ascii_letters+string.digits+string.punctuation,k=length))
-print("Generated password:",password)
