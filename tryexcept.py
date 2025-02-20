2da2a45c881af393e3a31bff127ae6820606d20d
Geethika-taashee <geetika.a@taashee.com>
2025-02-20 17:04:54 +0530

    Delete old file

--- tryexcept.py
+++
@@ -1,6 +0,0 @@
-try:
-    user_input=int(input("Enter a number:"))
-    result=2*user_input
-    print(f"Twice the input is: {result}")
-except ValueError:
-    print("Invalid input! please enter a valid integer.")
